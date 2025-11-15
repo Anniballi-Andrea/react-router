3a64fb8f87a9e6faa7325a08332f1c4509fc32ac
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-15 18:05:19 +0100

    add same css and finish work

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -30,12 +30,13 @@ export default function Products() {
                                 <div key={el.id} className="col-sm-12 col-md-4 ">
                                     <div className="card">
                                         <img src={el.image} alt="" />
-                                        <div className="card-body">
-                                            <h4>{el.title}</h4>
-                                            <p><span>category:</span>{el.category} </p>
-                                            <p>{el.description} </p>
-                                            <p><span>-{el.price}</span></p>
-                                        </div>
+                                    </div>
+                                    <div className="card-body">
+                                        <h4>{el.title}</h4>
+                                        <p><span>category:</span>{el.category} </p>
+                                        <p>{el.description} </p>
+                                        <p><span>-{el.price}</span></p>
+
                                     </div>
                                 </div>
 
