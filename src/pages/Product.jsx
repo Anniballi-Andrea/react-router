3b6d5cedc31e1717952ce220975b04c55abe36d0
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-17 12:51:10 +0100

    add the fetch of single product with the same id of the path

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function Product() {
 
     const { id } = useParams()
+    const [prodoct, setProdoct] = useState({})
+
+    function fetchProdoct() {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((res) => setProdoct(res.data))
+
+
+
+
+
+    }
+
+    useEffect(fetchProdoct, [prodoct])
 
 
     return (
         <main>
-            <h1>Prodotto numero {id}</h1>
+            <img src={prodoct.image} alt="" />
         </main>
     )
 }
